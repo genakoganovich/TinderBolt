887aa13e0eee58bfe994f8f29f032d4a6dc42b77
Gennadiy Koganovich <genakoganovich@gmail.com>
2024-11-27 21:21:51 +0200

    0004: day 4 done

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def message_button(update, context):
 
     prompt = load_prompt(query)
     user_chat_history = "\n\n".join(dialog.list)
-    my_message = await send_text(update, context, "ChatGPT думает над вариантами ответа...")
+    my_message = await send_text(update, context, "ChatGPT 🧠 думает над вариантами ответа...")
     answer = await chatgpt.send_question(prompt, user_chat_history)
     await my_message.edit_text(answer)
 
@@ -90,6 +90,77 @@ async def message_button(update, context):
 async def message_dialog(update, context):
     text = update.message.text
     dialog.list.append(text)
+
+async def profile(update, context):
+    dialog.mode = "profile"
+    text = load_message("profile")
+    await send_photo(update, context, "profile")
+    await send_text(update, context, text)
+
+    dialog.user.clear()
+    dialog.count = 0
+    await send_text(update, context, "Сколько Вам лет?")
+
+
+async def profile_dialog(update, context):
+    text = update.message.text
+    dialog.count += 1
+    if dialog.count == 1:
+        dialog.user["age"] = text
+        await send_text(update, context, "Кем Вы работаете?")
+    elif dialog.count == 2:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "У Вас есть хобби?")
+    elif dialog.count == 3:
+        dialog.user["hobby"] = text
+        await send_text(update, context, "Что Вам не нравится в людях?")
+    elif dialog.count == 4:
+        dialog.user["annoys"] = text
+        await send_text(update, context, "Цель знакомства?")
+    elif dialog.count == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+
+        my_message = await send_text(update, context, "ChatGPT 🧠 занимается генерацией Вашего профиля. Подождите пару секунд...")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+
+async def opener(update, context):
+    dialog.mode = "opener"
+    text = load_message("opener")
+    await send_photo(update, context, "opener")
+    await send_text(update, context, text)
+
+    dialog.user.clear()
+    dialog.count = 0
+    await send_text(update, context, "Имя девушки?")
+
+async def opener_dialog(update, context):
+    text = update.message.text
+    dialog.count += 1
+    if dialog.count == 1:
+        dialog.user["name"] = text
+        await send_text(update, context, "Сколько ей лет?")
+    elif dialog.count == 2:
+        dialog.user["age"] = text
+        await send_text(update, context, "Оцените ее внешность: 1-10 баллов?")
+    elif dialog.count == 3:
+        dialog.user["handsome"] = text
+        await send_text(update, context, "Кем она работает?")
+    elif dialog.count == 4:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Цель знакомства?")
+    elif dialog.count == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("opener")
+        user_info = dialog_user_info_to_str(dialog.user)
+
+        answer = await chatgpt.send_question(prompt, user_info)
+        await send_text(update, context, answer)
+
+
 
 
 # тут будем писать наш код :)
@@ -100,6 +171,10 @@ async def hello(update, context):
         await date_dialog(update, context)
     if dialog.mode == "message":
         await message_dialog(update, context)
+    if dialog.mode == "profile":
+        await profile_dialog(update, context)
+    if dialog.mode == "opener":
+        await opener_dialog(update, context)
     else:
         await send_text(update, context, "*Привет*")
         await send_text(update, context, "_Как дела?_")
@@ -123,13 +198,17 @@ async def hello_button(update, context):
 dialog = Dialog()
 dialog.mode = None
 dialog.list = []
+dialog.count = 0
+dialog.user = {}
 
-chatgpt = ChatGptService(token="gpt:token")
+chatgpt = ChatGptService(token="gpt")
 app = ApplicationBuilder().token("token").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("date", date))
 app.add_handler(CommandHandler("message", message))
+app.add_handler(CommandHandler("profile", profile))
+app.add_handler(CommandHandler("opener", opener))
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 
